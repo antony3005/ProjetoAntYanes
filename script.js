6277a1bd545f315f17928323e443ccb283dcc06a
Antony <telesantony51@gmail.com>
2026-03-31 18:08:32 -0300

    Ligado com o banco, mas falta funcoes para setar e pegar valores

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDk4ecjc_nUqIDpwk8KcwvGs40j-NAnA0M",
+  authDomain: "projetoantyanes.firebaseapp.com",
+  databaseURL: "https://projetoantyanes-default-rtdb.firebaseio.com",
+  projectId: "projetoantyanes",
+  storageBucket: "projetoantyanes.firebasestorage.app",
+  messagingSenderId: "269109133484",
+  appId: "1:269109133484:web:fb6da20f3159d8598e1f09",
+  measurementId: "G-GGM594RS8P"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
+const distancia = ref(db, "dispositivo/sensor/distancia");
+const status = ref(db, "dispositivo/sensor/status");
+
 const email = document.getElementById ('email');
 const senha = document.getElementById ('senha');
 
